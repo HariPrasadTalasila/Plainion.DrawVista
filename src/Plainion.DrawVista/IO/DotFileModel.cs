62f9984e28c7a76d052cb98396feb981c336c291
HariPrasadTalasila <hariprasad.talasila@gmail.com>
2024-05-03 05:21:56 +0530

    Fixed isolated pages w/o references not part of Auto Index svg

--- src/Plainion.DrawVista/IO/DotFileModel.cs
+++ src/Plainion.DrawVista/IO/DotFileModel.cs
@@ -5,13 +5,18 @@ namespace Plainion.DrawVista.UseCases;
 public class DotFileModel(IDictionary<string, IList<string>> pageToReferencesMap)
 {
     private readonly IDictionary<string, IList<string>> myPageToReferencesMap = pageToReferencesMap;
-    
+
     public void WriteTo(string file)
     {
         StringBuilder graphContent = new();
 
         foreach (var (pageName, pageReferences) in myPageToReferencesMap)
         {
+            if(!pageReferences.Any())
+            {
+                graphContent.AppendLine($"{pageName}");
+            }
+
             foreach(var referencedPage in pageReferences)
             {
                 graphContent.AppendLine($"{pageName} -> {referencedPage}");
